6bd05310ca41f198374fc671cd5aaed2f51adccb
Eenyi <syedinshal62@gmail.com>
2023-01-28 21:21:30 +0500

    Add Student Record Done

--- app/src/main/java/com/example/savesabaq/RecordView.java
+++ app/src/main/java/com/example/savesabaq/RecordView.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +20,18 @@ public class RecordView extends AppCompatActivity implements View.OnClickListene
     TextView recordHolder, rollnoHolder;
     ImageButton addPerRecord, homeNavigation;
     DBHelper db;
+    Integer currentStudentRollNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_view);
         intent = getIntent();
+        db = new DBHelper(RecordView.this);
         recordHolder = findViewById(R.id.recordHolder);
         recordHolder.setText(intent.getStringExtra("student"));
         rollnoHolder = findViewById(R.id.rollnoHolder);
         rollnoHolder.setText( " رول نمبر  :  " + intent.getStringExtra("rollNumber"));
+        currentStudentRollNumber = Integer.parseInt(intent.getStringExtra("rollNumber"));
         addPerRecord = findViewById(R.id.addPerRecord);
         addPerRecord.setOnClickListener(this);
         homeNavigation = findViewById(R.id.homeNavigation);
@@ -49,9 +53,11 @@ public class RecordView extends AppCompatActivity implements View.OnClickListene
                 addStudent_dialogue.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        if (!sabaq.getText().toString().equals("") || !manzil.getText().toString().equals("")) {
+                        if (!sabaq.getText().toString().equals("") && !manzil.getText().toString().equals("")) {
                             int sabaq_val = 0, manzil_val = 0;
                             try {
+                                Log.d("sabaq", sabaq.getText().toString());
+                                Log.d("manzil", manzil.getText().toString());
                                 sabaq_val = Integer.parseInt(sabaq.getText().toString());
                                 manzil_val = Integer.parseInt(manzil.getText().toString());
                                 if (sabaq_val <= 0 || manzil_val <= 0) {
@@ -61,8 +67,8 @@ public class RecordView extends AppCompatActivity implements View.OnClickListene
                                 else {
                                     //code here
                                     Record record = new Record(sabaq_val, sabaq_val - 1, manzil_val);
-                                    if (db.addRecord(record, Integer.parseInt(rollnoHolder.getText().toString()))) {
-                                        Toast.makeText(RecordView.this, "Student Enrolled!", Toast.LENGTH_SHORT).show();
+                                    if (db.addRecord(record, currentStudentRollNumber)) {
+                                        Toast.makeText(RecordView.this, recordHolder.getText()+"'s Sabaq recorded!", Toast.LENGTH_SHORT).show();
                                     }
                                     else {
                                         Toast.makeText(RecordView.this, "Error occured in enrolling!", Toast.LENGTH_SHORT).show();
